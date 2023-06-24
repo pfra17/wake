076ac2abb886c451c97216df6a1ec4ebc058f444
Philipp Frase <frase_17@hotmail.com>
2023-06-24 23:53:58 +0200

    Add -a option to change settings with and without charger connected

--- wake.sh
+++ wake.sh
@@ -3,10 +3,10 @@
 SUDO=""
 (( $EUID > 0 )) && SUDO="sudo -n"
 function finish {
-    $SUDO pmset disablesleep 0
+    $SUDO pmset -a disablesleep 0
     printf "\nSleep has been re-enabled"
 }
-$SUDO pmset disablesleep 1 || exit
+$SUDO pmset -a disablesleep 1 || exit
 trap finish EXIT
 printf "Sleep disabled... quit script to re-enable (Ctrl+C)\n"
 while :
